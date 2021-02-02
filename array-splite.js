b91484db634c8cb7701786873f34b77b5dd12cdd
Md Nayem Hossain <2191081070@uttarauniversity.edu.bd>
2021-02-02 20:34:39 +0600

    slice splice join

---
+++ array-splite.js
@@ -0,0 +1,12 @@
+const num = [1,2,3,4,5,6,7,8,9];
+const sliceNum = num.slice(3 , 6);
+console.log(sliceNum);
+console.log(num);
+
+const num1 = [1,2,3,4,5,6,7,8,9];
+const spliceNum = num1.splice(2 , 4, 89,98,90,97,96,94);
+console.log(spliceNum);
+console.log(num1);
+
+const allNum = num1.join(' Nayem ');
+console.log(allNum);
