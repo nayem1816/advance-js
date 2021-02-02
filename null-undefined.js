a8fb0a78f1d0b0e2d68799754ae52188b34396ab
Md Nayem Hossain <2191081070@uttarauniversity.edu.bd>
2021-02-02 15:12:05 +0600

    null-undefined

---
+++ null-undefined.js
@@ -0,0 +1,35 @@
+// let num;
+// console.log(num);
+
+// function add(num1 , num2) {
+//     console.log(num1 + num2);     // not return 
+// }
+
+// function add(num1 , num2) {
+//     console.log(num1 + num2);     // return but not value
+//     return; 
+// }
+
+
+function add(num1 , num2) {
+    console.log(num1 , num2);
+}
+const result = add(10);
+console.log(result);
+
+
+const mobile = {samsung:'A71', nokia:'N71', apple:'iphone-10'};
+console.log(mobile.huawei);
+
+
+const num = undefined;
+console.log(num);
+
+
+
+// null -> Null is a keyword in JavaScript that signifies no value of any value.;
+
+
+
+
+
