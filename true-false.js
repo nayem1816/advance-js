7e0383f7dfb0c24e2d98708c7780e80098659ec6
Md Nayem Hossain <2191081070@uttarauniversity.edu.bd>
2021-02-02 14:41:35 +0600

    true-false js

---
+++ true-false.js
@@ -0,0 +1,22 @@
+//False Value
+// 0
+// undefine
+// Null 
+// NaN 
+// false 
+// ''
+
+//True Value
+// ' ' --- inverted comma inside white space
+// '0' --- inverted comma inside Zero
+// {}
+// []
+// ETC..........
+
+
+const num = 0;
+if (num) {
+    console.log('True');
+} else {
+    console.log('False');
+}
